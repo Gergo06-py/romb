58afdf0f372600eb84d3511dba6961eb6d3c0509
Gergo06-py <borbelygergo06@gmail.com>
2024-01-10 10:38:24 +0100

    Teszt2

--- src/test/java/RombuszTest.java
+++ src/test/java/RombuszTest.java
@@ -1,5 +1,18 @@
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
 import com.example.Rombusz;
 
 public class RombuszTest {
     Rombusz rombusz;
+
+    @BeforeTest
+    public void setup() {
+        rombusz = new Rombusz();
+    }
+
+    @Test
+    public void calcArea() {
+        rombusz.calcArea();
+    }
 }
