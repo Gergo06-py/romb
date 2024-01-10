4e2fd74bbc26b816d81eb4ee02739fe46a596c76
Gergo06-py <borbelygergo06@gmail.com>
2024-01-10 11:08:57 +0100

    Teszt12

--- src/main/java/com/example/Rombusz.java
+++ src/main/java/com/example/Rombusz.java
@@ -2,6 +2,8 @@ package com.example;
 
 public class Rombusz {
     public double calcArea(double side, double deg) {
+        if (side == 0)
+            throw new IllegalArgumentException();
         double rad = deg / 180 * Math.PI;
         return Math.pow(side, 2) * Math.sin(rad);
     }
